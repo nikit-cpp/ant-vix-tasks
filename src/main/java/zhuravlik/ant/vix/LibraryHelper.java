94bdc691af396f6e9a6456fcc43c9d27c2d9dc46
Konev.Nikita <Konev.Nikita@MSK-W0328.infotecs-nt>
2014-09-08 16:27:02 +0400

    восстановлена работа на Win7 x64 - 32-bit java -- путь к vix.dll нужно указывать вручную как в примере build.xml

--- src/main/java/zhuravlik/ant/vix/LibraryHelper.java
+++ src/main/java/zhuravlik/ant/vix/LibraryHelper.java
@@ -42,116 +42,17 @@ public class LibraryHelper {
     
     public static Vix getInstance() {
         if (instance == null) {
-            setLibraryPath(path);
             instance = loadVixLibrary();            
         }
         
         return instance;
-    }
-
-    /*
-     * Sets JNA library path system property (jna.library.path) to the location
-     * of VIX shared library.
-     *
-     * If user specified it for head task, user-specified value will be used.
-     * If user did not specify it, it will be automatically guessed.
-     *
-     * @param path User-specified path
-     */
-    public static void setLibraryPath(String path) {
-
-        if (path != null) {
-            // user-specified path has highest priority
-
-            System.setProperty("jna.library.path", path);
-        }
-        else if (SystemUtils.IS_OS_WINDOWS) {
-            // on Windows, there is no easy location of library
-
-            // either 32 bit or 64 bit vmware
-            String path1 = "C:/Program Files/VMware/VMware VIX/";
-            String path2 = "C:/Program Files (x86)/VMware/VMware VIX/";
-
-            // final path
-            String gpath;
-
-            if (new File(path1).exists()) {
-                // 32 bit vix on 32 bit sys or 64 bit vix on 64 bit sys
-
-                if (!SystemUtils.JAVA_VM_NAME.contains("64")
-                        &&
-                        new File("C:/Program Files (x86)").exists()) {
-                    // 32 bit java on 64 bit sys
-
-                    if (new File(path2).exists()) {
-                        // 32 bit vix, 32 bit Java
-                        gpath = path2;
-                    }
-                    else {
-                        // 64 bit vix, but 32 bit Java
-                        throw new BuildException("Cannot use 64-bit VMWare libraries with 32-bit Java Runtime." +
-                                " Either install 64-bit Java or VMWare with 32 bit library support");
-                    }
-                }
-                else
-                    gpath = path1;
-            }
-            else {
-                // 32 bit vix on 64 bit system
-
-                if (SystemUtils.JAVA_VM_NAME.contains("64")) {
-                    // 64 bit Java
-                    throw new BuildException("Cannot use 32-bit VMWare libraries with 64-bit Java Runtime." +
-                            " Either install 32-bit Java or VMWare with 64 bit support (8+).");
-                }
-                else if (new File(path2).exists()) {
-                    // 32 bit Java, vmware path exists
-                    gpath = path2;
-                }
-                else {
-                    // 32 bit Java, vmware path not exists
-                    throw new BuildException("Cannot locate any VMWare folder, ensure it is installed. " +
-                            "If it is, please file a bug with OS version and VMWare product name and version");
-                }
-            }
-
-            // iterate over vmware directory to find libraries for specific product
-            File dir = new File(gpath);
-            String[] list = dir.list();
-            for (String fl: list)
-            {
-                //if (fl.contains("Workstation-") || fl.contains("Server-")
-                //        || fl.contains("Player-") || fl.contains("VIServer-"))
-
-                if (
-                        provider.equals("vi") && fl.contains("VIServer-") ||
-                        provider.contains("workstation") && fl.contains("Workstation-") ||
-                        provider.equals("player") && fl.contains("Player-") ||
-                        provider.equals("server") && fl.contains("Server-")
-                   )
-                {
-                    File wdir = new File(gpath + fl);
-                    String[] wlist = wdir.list();
-                    for (String pfl: wlist)
-                    {
-                        if (pfl.equals("64bit") || pfl.equals("32bit")) {
-                            System.setProperty("jna.library.path", gpath + fl + "/" + pfl);
-                        }
-                    }
-                }
-            }
-        }
-        else {
-            // for unix-like it should reside under /usr/lib/vmware-vix for all products
-
-            System.setProperty("jna.library.path", "/usr/lib/vmware-vix/");
-        }
     }
     
     public static Vix loadVixLibrary() {
         if (new File("/usr/lib/vmware-vix/libvixAllProducts.so").exists())     // on Unix there is single entry point
             return (Vix) Native.loadLibrary("vixAllProducts", Vix.class);
         else
-            return (Vix) Native.loadLibrary("vix", Vix.class);     // on Windows there is vix.dll
+            // return (Vix) Native.loadLibrary("vix", Vix.class);     // on Windows there is vix.dll
+            return (Vix) Native.loadLibrary(path, Vix.class);     // on Windows there is vix.dll
     }
 }
